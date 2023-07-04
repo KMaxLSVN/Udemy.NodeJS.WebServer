aedb4f374a89eb6a2c70e926ee13ef35b67a1f9b
Max Kim <maxim250291@gmail.com>
2023-07-04 20:46:35 +0300

    add humidity parameter

--- src/utils/weatherstack.js
+++ src/utils/weatherstack.js
@@ -21,7 +21,7 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
   const token = '2f38e587aeddb44c61979b4f377e4855';
-  const url = `http://api.weatherstack.com/current?access_key=${token}&query=${latitude},${longitude}&units=s`;
+  const url = `http://api.weatherstack.com/current?access_key=${token}&query=${latitude},${longitude}&units=m`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -30,10 +30,10 @@ const forecast = (latitude, longitude, callback) => {
       callback(body, undefined);
     } else {
       const description = body.current.weather_descriptions[0];
-      const { temperature, feelslike } = body.current;
+      const { temperature, feelslike, humidity } = body.current;
       callback(
         undefined,
-        `${description}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`
+        `${description}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out. The humidity is ${humidity}%.`
       );
     }
   });
